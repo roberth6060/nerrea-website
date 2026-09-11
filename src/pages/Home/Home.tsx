eb5cd98f9c946642208e6e9dce64fc039914e47f
Robert C. Haye <rch4538@g.rit.edu>
2026-09-11 10:58:11 +0200

    add responsive hero assets to home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,15 @@
+import Hero from "../../components/Hero/hero";
+
 const Home = () => {
   return (
     <section className="home">
       <h1>Welcome to Nerrea</h1>
+      <Hero
+        heading="Your virtual Assistant for business"
+        supportingText=""
+        ctas=""
+      />
+
       <p>My Home page.</p>
     </section>
   );
